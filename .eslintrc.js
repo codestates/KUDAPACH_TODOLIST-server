bb66dc552ca557e1c0e66409be54d067bb6d4753
Sanghyuk Choi <nugu1223@gmail.com>
2021-02-02 02:00:50 +0900

    boiler plate

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,17 @@
+/*eslint-disable*/
 module.exports = {
-  "extends": [
-    "eslint:recommended", // eslint 권장사항 적용
-    "plugin:prettier/recommended" // eslint, prettier 연결
+  extends: [
+    'eslint:recommended', // eslint 권장사항 적용
+    'plugin:prettier/recommended', // eslint, prettier 연결
   ],
-  "rules": {
-    "no-console": "error", // 콘솔로그 error 처리
-    "semi": ["error", "always"] // ; 반드시 존재해야 함
+  rules: {
+    'no-console': ['error'], // 콘솔로그 error 처리
+    semi: ['error', 'always'], // ; 반드시 존재해야 함
   },
-  "parserOptions": {
-    "ecmaVersion": 2017
+  parserOptions: {
+    ecmaVersion: 2017,
   },
-  "env": {
-    "es6": true
-  }
-}
+  env: {
+    es6: true,
+  },
+};
