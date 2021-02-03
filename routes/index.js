b1b2762ee9fc2d49709749b36d0aca1c3f33d29d
paigekim29 <paigekim29@gmail.com>
2021-02-03 23:40:09 +0900

    [task06] Update router

--- routes/index.js
+++ routes/index.js
@@ -8,30 +8,27 @@ const { controllers } = require('../controllers');
 router.post('/signin', controllers.signin.post);
 
 // * POST https://www.kudapach.com/signup
-router.post('/signin', controllers.signup.post);
-
-// * GET https://www.kudapach.com/userinfo
-router.post('/signin', controllers.user.post);
+router.post('/signup', controllers.signup.post);
 
 // * GET https://www.kudapach.com/user/info
-router.post('/signin', controllers.user.post);
+router.get('/user/info', controllers.user.post);
 
 // * POST https://www.kudapach.com/user/info/edit
-router.post('/signin', controllers.user.info.post);
+router.post('/user/info/edit', controllers.user.edit);
 
 // * GET https://www.kudapach.com/todo
-router.post('/signin', controllers.todo.post);
+router.get('/todo', controllers.todo.post);
 
 // * POST https://www.kudapach.com/todo/edit
-router.post('/signin', controllers.todo.post);
+router.post('/todo/edit', controllers.todo.edit);
 
 // * POST https://www.kudapach.com/todo/calendar
-router.post('/signin', controllers.todo.post);
+router.post('/todo/calendar', controllers.todo.calendar);
 
 // * POST https://www.kudapach.com/group
-router.post('/signin', controllers.group.post);
+router.post('/group', controllers.group.post);
 
-// * POST https://www.kudapach.com/group/?groupid
-router.post('/signin', controllers.group.post);
+// * POST https://www.kudapach.com/group/:groupid
+router.post('/group/:groupid', controllers.group.redirect);
 
 module.exports = router;
