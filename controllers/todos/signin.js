31ef34803510164d2e6f9eaeb1e1ab37290cc7b7
Sanghyuk Choi <nugu1223@gmail.com>
2021-02-04 20:38:19 +0900

    [task11] Update controller signin.js & Create tokenfunction
    Sign in 방식을 토큰으로 변경하였습니다.
    tokenfunction이 새로 생겼습니다.

--- controllers/todos/signin.js
+++ controllers/todos/signin.js
@@ -1,27 +1,34 @@
-const { users } = require('../../models');
+const { user } = require('../../models');
+const {
+  generateAccessToken,
+  generateRefreshToken,
+  sendRefreshToken,
+  sendAccessToken,
+} = require('../tokenfunction');
+
 module.exports = {
   post: async (req, res) => {
-    let userInfo = await users.findOne({
-      where: { email: req.body.email, password: req.body.password },
-    });
-    if (userInfo) {
-      res
-        .status(200)
-        .cookie('email', userInfo.eamil, {
-          domain: 'https://www.kudapach.com',
-          path: '/',
-          secure: true,
-          httpOnly: true,
-          sameSite: 'none',
-        })
-        .send({
-          email: userInfo.email,
-          username: userInfo.username,
-          group: false,
-        })
-        .catch((err) => res.status(500).send(err));
-    } else {
-      res.status(401).send('Invalid email or wrong password');
-    }
+    const { email, password } = req.body;
+    await user
+      .findOne({
+        where: {
+          email,
+          password,
+        },
+      })
+      .then((data) => {
+        if (!data) {
+          return res.status(401).send('Invalid email or wrong password');
+        }
+        delete data.password;
+        const accessToken = generateAccessToken(data);
+        const refreshToken = generateRefreshToken(data);
+
+        sendRefreshToken(res, refreshToken);
+        sendAccessToken(res, accessToken, 'Sign in successful!');
+      })
+      .catch((err) => {
+        res.status(500).send(err);
+      });
   },
 };
