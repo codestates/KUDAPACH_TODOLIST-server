2f232d0781c27571d0ca3db2af7d2cc360852797
paigekim29 <paigekim29@gmail.com>
2021-02-04 23:33:27 +0900

    [task06]Update routes
    aws에서 server domain을 수정하였기 때문에 다시 수정하였습니다

--- routes/index.js
+++ routes/index.js
@@ -4,31 +4,31 @@ const router = express.Router();
 
 const { controllers } = require('../controllers');
 
-// * POST https://www.kudapach.com/signin
+// * POST https://server.kudapach.com/signin
 router.post('/signin', controllers.signin.post);
 
-// * POST https://www.kudapach.com/signup
+// * POST https://server.kudapach.com/signup
 router.post('/signup', controllers.signup.post);
 
-// * GET https://www.kudapach.com/user/info
+// * GET https://server.kudapach.com/user/info
 router.get('/user/info', controllers.user.get);
 
-// * POST https://www.kudapach.com/user/info/edit
+// * POST https://server.kudapach.com/user/info/edit
 router.post('/user/info/edit', controllers.user.edit);
 
-// * GET https://www.kudapach.com/todo
+// * GET https://server.kudapach.com/todo
 router.get('/todo', controllers.todo.get);
 
-// * POST https://www.kudapach.com/todo/edit
+// * POST https://server.kudapach.com/todo/edit
 router.post('/todo/edit', controllers.todo.edit);
 
-// * POST https://www.kudapach.com/todo/calendar
+// * POST https://server.kudapach.com/todo/calendar
 router.post('/todo/calendar', controllers.todo.calendar);
 
-// * GET https://www.kudapach.com/group
+// * GET https://server.kudapach.com/group
 router.get('/group', controllers.group.get);
 
-// * POST https://www.kudapach.com/setting/group/edit
+// * POST https://server.kudapach.com/setting/group/edit
 router.post('/group/:groupid', controllers.setting.post);
 
 module.exports = router;
