50ca9a5c7591fc43ed06047ababe4bc76c79a150
paigekim29 <paigekim29@gmail.com>
2021-02-05 13:42:30 +0900

    [task14]user.js
    my page를 수정하는 액션이 일어났을 때, 필요한 코드를 짰습니다.

--- controllers/todos/user.js
+++ controllers/todos/user.js
@@ -15,9 +15,10 @@ module.exports = {
           res.status(401).send('access token has been tempered');
         }
 
-        const { email, username, mobile } = data;
+        const { id, email, username, mobile } = data;
         res.status(200).json({
           data: {
+            id,
             email,
             username,
             mobile,
@@ -27,5 +28,22 @@ module.exports = {
       .catch((err) => {
         res.status(500).send(err);
       });
+  },
+
+  edit: async (req, res) => {
+    const { id, username, mobile, password } = req.body;
+    user
+      .update({ username, mobile, password }, { where: { id } })
+      .then((data) => {
+        const { id, username, mobile } = data;
+        res.status(200).json({
+          data: {
+            id,
+            username,
+            mobile,
+          },
+        });
+      })
+      .catch((err) => res.status(500).send(err));
   },
 };
