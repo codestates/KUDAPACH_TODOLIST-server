653f94becdad8c8b0b618d61dad08fc037e462f6
Sanghyuk Choi <nugu1223@gmail.com>
2021-02-08 21:32:49 +0900

    notTask & Update

--- migrations/20210203151705-create-users-groups.js
+++ migrations/20210203151705-create-users-groups.js
@@ -26,7 +26,19 @@ module.exports = {
         },
         onDelete: 'cascade',
         onUpdate: 'cascade',
-      }
+      },
+      createdAt: {
+        allowNull: false,
+        type: 'TIMESTAMP',
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: 'TIMESTAMP',
+        defaultValue: Sequelize.literal(
+          'CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP',
+        ),
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
