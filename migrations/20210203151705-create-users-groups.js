f950f2018eac76ff27e53ff8ce7496cd01786aac
Sanghyuk Choi <nugu1223@gmail.com>
2021-02-08 14:18:31 +0900

    [task22] Update schema
    제발... 마지막 스키마가 되길..

--- migrations/20210203151705-create-users-groups.js
+++ migrations/20210203151705-create-users-groups.js
@@ -3,6 +3,12 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('users_groups', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
       userid: {
         type: Sequelize.INTEGER,
         references: {
